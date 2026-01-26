1b3d407cceef2ce431852890674d6959a86c6dcf
Arulselvan M <arulselvan2244@gmail.com>
2026-01-26 22:33:56 +0530

    Updated contracts

---
+++ src/core/SubmissionRegistry.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+import {ISubmissionRegistry} from "../interfaces/ISubmissionRegistry.sol";
+
+contract SubmissionRegistry is ISubmissionRegistry {
+
+}
---
+++ src/core/VerificationRegistry.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.28;
+
+
+import {IVerificationRegistry} from "../interfaces/IVerificationRegistry.sol";
+
+contract VerificationRegistry is IVerificationRegistry {
+
+}
